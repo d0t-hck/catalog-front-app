5c647747ac46078111d73c7f3a58aea46ffee554
d0t-hck <42749949+d0t-hck@users.noreply.github.com>
2022-03-10 21:32:36 +0600

    add 'SideBarIcon' and 'SideBar' components

---
+++ src/components/SideBar/SideBar.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import SideBarIcon from './SideBarIcon.vue';
+import home from '../../assets/home.svg?raw';
+import add from '../../assets/add.svg?raw';
+</script>
+
+<template>
+    <div class="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg">
+        <SideBarIcon :icon="home" text="Home" />
+        <SideBarIcon :icon="add" text="Add" />
+    </div>
+</template>
---
+++ src/components/SideBar/SideBarIcon.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { PropType } from 'vue';
+    defineProps<{ icon: string, text: string }>();
+</script>
+
+<template>
+    <div class="sidebar-icon group">
+        <!-- <inline-svg :src=icn /> -->
+        <svg class="filter-green" v-html="icon" v-inline />
+        <span class="sidebar-tooltip group-hover:scale-100">
+            {{ text }}
+        </span>
+    </div>
+</template>
+
+<style>
+    .filter-green{
+        filter: invert(48%) sepia(79%) saturate(2476%) hue-rotate(86deg) brightness(118%) contrast(119%);
+    }
+    svg {
+        fill: #ffffff!important;
+        stroke: #ffffff!important;
+    }
+</style>
