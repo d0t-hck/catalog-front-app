212d7eedbb0996d6342a84b9702b9af2a438a228
d0t-hck <42749949+d0t-hck@users.noreply.github.com>
2022-03-10 21:30:14 +0600

    add custom 'inline' directive

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.directive("inline", (element) => {
+    element.replaceWith(...element.children);
+});
+
+app.mount('#app');
