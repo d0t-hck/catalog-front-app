0ea7cdb2f64e3bd322533861cc7f1cd997166283
d0t-hck <42749949+d0t-hck@users.noreply.github.com>
2022-03-14 21:52:52 +0600

    axios client for creator endpoint

---
+++ src/services/creators.api.ts
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+const axiosClient = axios.create({
+    baseURL: 'http://localhost:8000/api/creator',
+});
+
+export async function getCreators() {
+    try {
+        const { data } = await axiosClient.get('/');
+        return [null, data];
+    } catch (error) {
+        return [error];
+    }
+}
+
+export async function getCreator(id: number) {
+    try {
+        const { data } = await axiosClient.get(`/${id}`);
+        return [null, data];
+    } catch (error) {
+        return [error];
+    }
+}
+
+export async function updateCreator(id: number, creator: Partial<Creator>) {
+    try {
+        const { data } = await axiosClient.put(`/${id}`, creator);
+        return [null, data];
+    } catch (error) {
+        return [error];
+    }
+}
+
+export async function deleteCreator(id: number) {
+    try {
+        await axiosClient.delete(`/${id}`);
+    } catch (error) {
+        return [error];
+    }
+}
