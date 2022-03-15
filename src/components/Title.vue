3ffffa7920bfb826c4deb19e0398a7ac605ed6d7
d0t-hck <42749949+d0t-hck@users.noreply.github.com>
2022-03-15 21:38:14 +0600

    Title component

---
+++ src/components/Title.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { Title } from '../services/titles.api';
+defineProps<{ title: Title }>();
+</script>
+
+<template>
+    <div class="title">
+        <div class="poster-wrapper">
+            <!-- <img :src="`http://localhost:8000/public/images/${title.normalized_name}/poster.png`" /> -->
+        </div>
+        <p>{{title.name}}</p>
+        <p>{{title.release_year}}</p>
+    </div>
+</template>
