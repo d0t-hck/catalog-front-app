f52edaa73395c91bfccf689ef741e1f265fb2cd0
d0t-hck <42749949+d0t-hck@users.noreply.github.com>
2022-03-15 21:39:38 +0600

    use 'Title' component

---
+++ src/components/Home.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+// defineProps<{ id: number }>();
+</script>
+
+<template>
+    <div class="content-container">
+        <div class="content-list">
+            <Title v-for="title in titles" :title="title"/>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Title from './Title.vue';
+import { Title as model } from '../services/titles.api';
+import { getTitles } from '../services/titles.api';
+
+export default {
+    data() {
+        return {
+            titles: Array<model>(),
+        };
+    },
+    async created() {
+        const [error, data] = await getTitles();
+        if (error) console.error(error);
+        else this.titles = data;
+    }
+}
+</script>
